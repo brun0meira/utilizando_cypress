6f43d37c434cd828b77172d47856997284dd0335
brun0meira <brunomeira321@gmail.com>
2024-03-11 00:17:08 -0300

    feat: add project cypress

---
+++ cypress/support/commands.ts
@@ -0,0 +1,28 @@
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Get one or more DOM elements by test id.
+     *
+     * @param id The test id
+     * @param options The same options as cy.get
+     */
+    byTestId<E extends Node = HTMLElement>(
+      id: string,
+      options?: Partial<
+        Cypress.Loggable & Cypress.Timeoutable & Cypress.Withinable & Cypress.Shadow
+      >,
+    ): Cypress.Chainable<JQuery<E>>;
+  }
+}
+
+Cypress.Commands.add(
+  'byTestId',
+  (
+    id: string,
+    options?: Partial<
+      Cypress.Loggable & Cypress.Timeoutable & Cypress.Withinable & Cypress.Shadow
+    >,
+  ) => {
+    return cy.get(`[data-testid="${id}"]`, options);
+  },
+);
